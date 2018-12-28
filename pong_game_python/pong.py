7d63185031596af8b302c6f18a3f5325eac1e440
devyohan <ticonweb@gmail.com>
2018-12-28 15:13:22 +0900

    B 플레이어 막대기

--- pong_game_python/pong.py
+++ pong_game_python/pong.py
@@ -15,6 +15,15 @@ paddle_a.shapesize(stretch_wid = 5, stretch_len = 1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
 # 메인 게임 코드
 while True:
     wn.update()
